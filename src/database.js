77b72c57f8bf6cd0e13b4788c690efd57812f2fe
David Basantes <sebastian2405lucero@hotmail.com>
2023-06-24 13:09:54 -0500

    Deploymeny

--- src/database.js
+++ src/database.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 
 const MONGODB_URI = 'mongodb+srv://grupo2:FeNUdVSzIN2L3L1C@cluster0.jmb51re.mongodb.net/?retryWrites=true&w=majority'
 
-
 connection = async()=>{
     try {
          await mongoose.connect(MONGODB_URI,{
