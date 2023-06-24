9f85edef542604176022b16e424c79856ba083cd
David Basantes <sebastian2405lucero@hotmail.com>
2023-06-24 13:05:15 -0500

    Deployment

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ require('dotenv').config() //ESTO SIEMPRE VA PRIMERO
 const app = require('./server.js')
 const connection= require("./database.js")
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(app.get("port"), () => {
+  console.log(`Server on port ${app.get("port")}`);
+});
 
 
 connection()
