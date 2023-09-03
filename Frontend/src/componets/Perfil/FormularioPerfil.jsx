a0927a83209f8d219b375a687808a9b8934caadf
David Basantes <sebastian2405lucero@hotmail.com>
2023-09-03 11:49:46 -0500

    Estructure Project Finished

--- Frontend/src/componets/Perfil/FormularioPerfil.jsx
+++ Frontend/src/componets/Perfil/FormularioPerfil.jsx
@@ -1,65 +1,148 @@
+import { useContext, useState } from "react";
+import AuthContext from "../../context/AuthProvider";
+import Mensaje from "../Alertas/Mensaje";
 const FormularioPerfil = () => {
-    return (
-        <form>
+  const { auth, actualizarPerfil } = useContext(AuthContext);
+   const [mensaje, setMensaje] = useState({});
 
-            <div>
-                <label
-                    htmlFor='nombre'
-                    className='text-gray-700 uppercase font-bold text-sm'>Nombre: </label>
-                <input
-                    id='nombre'
-                    type="text"
-                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
-                    placeholder='nombre'
-                    name='nombre'
-                />
-            </div>
-            <div>
-                <label
-                    htmlFor='nombre'
-                    className='text-gray-700 uppercase font-bold text-sm'>Nombre: </label>
-                <input
-                    id='nombre'
-                    type="text"
-                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
-                    placeholder='nombre'
-                    name='nombre'
-                />
-            </div>
-            <div>
-                <label
-                    htmlFor='nombre'
-                    className='text-gray-700 uppercase font-bold text-sm'>Nombre: </label>
-                <input
-                    id='nombre'
-                    type="text"
-                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
-                    placeholder='nombre'
-                    name='nombre'
-                />
-            </div>
+   const handleSubmit = async (e) => {
+     e.preventDefault();
+     if (Object.values(form).includes("")) {
+       setMensaje({
+         respuesta: "Todos los campos deben ser ingresados",
+         tipo: false,
+       });
+       setTimeout(() => {
+         setMensaje({});
+       }, 3000);
+       return;
+     }
+     const resultado = await actualizarPerfil(form);
+     setMensaje(resultado);
+     setTimeout(() => {
+       setMensaje({});
+    }, 3000);
+   };
+  const [form, setform] = useState({
+    id: auth._id,
+    nombre: auth.nombre || "",
+    apellido: auth.apellido || "",
+    direccion: auth.direccion || "",
+    telefono: auth.telefono || "",
+    email: auth.email || "",
+  });
 
-            <div>
-                <label
-                    htmlFor='detalles'
-                    className='text-gray-700 uppercase font-bold text-sm'>Detalles: </label>
-                <textarea
-                    id='detalles'
-                    type="text"
-                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
-                    name='detalles'
-                />
-            </div>
+  const handleChange = (e) => {
+    setform({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-            <input
-                type="submit"
-                className='bg-gray-800 w-full p-3 
+  return (
+    <form onSubmit={handleSubmit}>
+      {Object.keys(mensaje).length > 0 && (
+        <Mensaje tipo={mensaje.tipo}>{mensaje.respuesta}</Mensaje>
+      )}
+      <div>
+        <label
+          htmlFor="nombre"
+          className="text-gray-700 uppercase font-bold text-sm"
+        >
+          Nombre:{" "}
+        </label>
+        <input
+          id="nombre"
+          type="text"
+          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5"
+          placeholder="nombre"
+          name="nombre"
+          value={form.nombre}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div>
+        <label
+          htmlFor="apellido"
+          className="text-gray-700 uppercase font-bold text-sm"
+        >
+          Apellido:{" "}
+        </label>
+        <input
+          id="apellido"
+          type="text"
+          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5"
+          placeholder="apellido"
+          name="apellido"
+          value={form.apellido}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div>
+        <label
+          htmlFor="direccion"
+          className="text-gray-700 uppercase font-bold text-sm"
+        >
+          Dirección:{" "}
+        </label>
+        <input
+          id="direccion"
+          type="text"
+          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5"
+          placeholder="direccion"
+          name="direccion"
+          value={form.direccion}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div>
+        <label
+          htmlFor="telefono"
+          className="text-gray-700 uppercase font-bold text-sm"
+        >
+          Teléfono:{" "}
+        </label>
+        <input
+          id="ditelefonoreccion"
+          type="text"
+          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5"
+          placeholder="telefono"
+          name="telefono"
+          value={form.telefono}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div>
+        <label
+          htmlFor="email"
+          className="text-gray-700 uppercase font-bold text-sm"
+        >
+          Email:{" "}
+        </label>
+        <input
+          id="email"
+          type="text"
+          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5"
+          placeholder="email"
+          name="email"
+          value={form.email}
+          onChange={handleChange}
+        />
+      </div>
+
+      <input
+        type="submit"
+        className="bg-gray-800 w-full p-3 
         text-slate-300 uppercase font-bold rounded-lg 
-        hover:bg-gray-600 cursor-pointer transition-all'
-                value='Actualizar' />
+        hover:bg-gray-600 cursor-pointer transition-all"
+        value="Actualizar"
+      />
+    </form>
+  );
+};
 
-        </form>
-    )
-}
-
-export default FormularioPerfil
+export default FormularioPerfil;
--- Frontend/src/context/AuthProvider.jsx
+++ Frontend/src/context/AuthProvider.jsx
@@ -21,6 +21,43 @@ const AuthProvider = ({ children }) => {
       console.log(error);
     }
   };
+  const actualizarPerfil = async (datos) => {
+    const token = localStorage.getItem("token");
+    try {
+      const url = `${import.meta.env.VITE_BACKEND_URL}/veterinario/${datos.id}`;
+      const options = {
+        headers: {
+          method: "PUT",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const respuesta = await axios.put(url, datos, options);
+      perfil(token);
+      return { respuesta: respuesta.data.msg, tipo: true };
+    } catch (error) {
+      return { respuesta: error.response.data.msg, tipo: false };
+    }
+  };
+  const actualizarPassword = async (datos) => {
+    const token = localStorage.getItem("token");
+    try {
+      const url = `${
+        import.meta.env.VITE_BACKEND_URL
+      }/veterinario/actualizarpassword`;
+      const options = {
+        headers: {
+          method: "PUT",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const respuesta = await axios.put(url, datos, options);
+      return { respuesta: respuesta.data.msg, tipo: true };
+    } catch (error) {
+      return { respuesta: error.response.data.msg, tipo: false };
+    }
+  };
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (token) {
@@ -33,6 +70,8 @@ const AuthProvider = ({ children }) => {
       value={{
         auth,
         setAuth,
+        actualizarPerfil,
+        actualizarPassword
       }}
     >
       {children}
