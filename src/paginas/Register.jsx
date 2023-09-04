24b7bf1d22202ec187babd89fe63ca155faf23f3
David Basantes <sebastian2405lucero@hotmail.com>
2023-09-04 14:33:30 -0500

    Render

--- src/paginas/Register.jsx
+++ src/paginas/Register.jsx
@@ -22,7 +22,7 @@ export const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const url = "http://localhost:3000/api/registro";
+      const url = ` ${import.meta.env.VITE_BACKEND_URL}/api/registro`;
       const respuesta = await axios.post(url, form);
       setMensaje({ respuesta: respuesta.data.msg, tipo: true });
       Swal.fire({
