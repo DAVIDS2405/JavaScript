e5507cf9c675d2f54011f87f3e946c9b94de650f
David Basantes <sebastian2405lucero@hotmail.com>
2023-09-04 10:43:30 -0500

    Nodemiler

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -33,16 +33,16 @@ const sendMailToUser = async(userMail,token)=>{
 // send mail with defined transport object
 const sendMailToRecoveryPassword = async(userMail,token)=>{
     let info = await transport.sendMail({
-    from: 'admin@vet.com',
-    to: userMail,
-    subject: "Correo para reestablecer tu contraseña",
-    html: `
+      from: "admin@vet.com",
+      to: userMail,
+      subject: "Correo para reestablecer tu contraseña",
+      html: `
     <h1>Sistema de gestión (VET-ESFOT 🐶 😺)</h1>
     <hr>
-    <a href="http://localhost:5173/recuperar-password/${token}">Clic para reestablecer tu contraseña</a>
+    <a href="https://frontend-poly-system.onrender.com/recuperar-password/${token}">Clic para reestablecer tu contraseña</a>
     <hr>
     <footer>Grandote te da la Bienvenida!</footer>
-    `
+    `,
     });
     console.log("Mensaje enviado satisfactoriamente: ", info.messageId);
 }
