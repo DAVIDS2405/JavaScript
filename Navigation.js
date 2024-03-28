490baca0c2e4a8a8abe6e9481d767f0b9cf5cf1b
David Basantes <85196670+DAVIDS2405@users.noreply.github.com>
2024-03-27 22:49:17 -0500

    Update and rename Objetos.js to Navigation.js

---
+++ Navigation.js
@@ -0,0 +1,32 @@
+const checkIsNavigationSupported = () => {
+  return Boolean(document.startViewTransition);
+};
+
+const fetchPage = async (url) => {
+  const response = await fetch(url);
+  const html = await response.text();
+  const data = html.match(/<body\b[^>]*>([\s\S]*?)<\/body>/i);
+  return data;
+};
+
+export const startViewTransition = () => {
+  if (checkIsNavigationSupported) return;
+
+  window.navigation.addEventListener("navigate", (event) => {
+    const toUrl = new URL(event.destination.url);
+
+    if (location.origin !== toUrl.origin) return;
+
+    event.intercept({
+      async handler() {
+        const data = await fetchPage(toUrl.pathname);
+        if (data) {
+          document.startViewTransition(() => {
+            document.body.innerHTML = data[1];
+            document.documentElement.scrollTop = 0;
+          });
+        }
+      },
+    });
+  });
+};
